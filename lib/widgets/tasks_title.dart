0e68e2e1ca9a22359f819bf2e1d43b3bd73b6132
pasha13d <mahabub13d1995@gmail.com>
2021-09-21 19:21:58 +0600

    add callback function for transfer setstate value stateful to stateless

--- lib/widgets/tasks_title.dart
+++ lib/widgets/tasks_title.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
 
-class TasksTitle extends StatelessWidget {
+class TasksTitle extends StatefulWidget {
+
+  @override
+  State<TasksTitle> createState() => _TasksTitleState();
+}
+
+class _TasksTitleState extends State<TasksTitle> {
+  bool isChecked = false;
+  // Callback function
+  void checkboxCallback(bool? checkboxState) {
+    if(checkboxState != null){
+      setState(() {
+        isChecked = checkboxState;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {
-
-        },
+      title: Text(
+          'This is a task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TasksCheckbox(checkboxState: isChecked, toggleCheckboxState: checkboxCallback),
+    );
+  }
+}
+
+class TasksCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final ValueChanged<bool?> toggleCheckboxState;
+  TasksCheckbox({required this.checkboxState, required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
+        // StatelessWidget we can't setState((){}); we can do it by using Callback
     );
   }
 }
